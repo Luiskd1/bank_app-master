2120e2c60a313b6f8121b5f2dae54fdc1d923302
Luiskd1 <125316846+Luiskd1@users.noreply.github.com>
2024-06-09 18:34:54 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -45,8 +45,6 @@ export default function Home() {
         <Link
           href="/auth/login"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          // target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
             Get Started{" "}
